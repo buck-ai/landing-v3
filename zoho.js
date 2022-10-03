6dbd7d21872766c5038f4acd37697dc7c6915ae1
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2022-10-04 02:44:20 +0300

    Add zoho widget

--- zoho.js
+++ zoho.js
@@ -1,14 +1,15 @@
-var $zoho = $zoho || {}
-$zoho.salesiq = $zoho.salesiq || { 
-  widgetcode: "08e2bc9e028e12192b43ec8fe0670f59fbe8368f78f523d134e0d795ae3d031e", 
-  values: {}
+var $zoho=$zoho || {}
+$zoho.salesiq = $zoho.salesiq || {
+  widgetcode:"08e2bc9e028e12192b43ec8fe0670f59fbe8368f78f523d134e0d795ae3d031e", 
+  values: {},
+  ready:function() { $zoho.salesiq.floatbutton.visible('hide') }
 }
 
-var d = document 
-s = d.createElement("script") 
-s.type = "text/javascript" 
-s.id = "zsiqscript" 
+var d = document
+s= d.createElement("script")
+s.type = "text/javascript"
+s.id = "zsiqscript"
 s.defer = true
-s.src = "https://salesiq.zoho.com/widget" 
-t = d.getElementsByTagName("script")[0] 
-t.parentNode.insertBefore(s, t)
+s.src="https://salesiq.zoho.com/widget"
+t = d.getElementsByTagName("script")[0]
+t.parentNode.insertBefore(s,t)
